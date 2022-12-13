42b14a5850b226d374fd7936d93ac60f1058497d
mGlvz <mogalvez@iu.edu>
2022-12-12 20:07:55 -0500

    testing

--- src/components/toDo/toDo.js
+++ src/components/toDo/toDo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import firebase from '../../utilities/firebase';
 import styles from "./toDo.css";
-import {Button} from 'react-bootstrap/Button';
+import {Button} from 'react-bootstrap';
 
 function ToDo({todo}){
     const completeToDo = () => {
@@ -22,41 +22,11 @@ function ToDo({todo}){
     return(
         <div>
             <h1 className={todo.complete ? 'complete' : 'incomplete'}>{todo.title}</h1>
-            <Button onClick={deleteToDo} variant="outline-secondary">Delete</Button>
-            <button className={todo.complete ? 'hide-button' : ''} onClick={completeToDo}>Complete</button>
-            <button className={todo.complete ? '' : 'hide-button'}  onClick={completeToDo}>Incomplete</button>
+            <Button onClick={deleteToDo} variant="secondary">Delete</Button> {}  
+            <Button className={todo.complete ? 'hide-button' : ''} onClick={completeToDo} variant="success">Complete</Button>
+            <Button className={todo.complete ? '' : 'hide-button'}  onClick={completeToDo} variant="danger">Incomplete</Button>
         </div>
     );
 }
 
 export default ToDo;
-
-// import {Button, Stack} from 'react-bootstrap';
-
-// function ToDo({todo}) {
-//     const completeToDo = () => {
-//         const todoRef = firebase.database().ref('crud-app').child(todo.id);
-
-//         todoRef.update({
-//             complete:!todo.complete,
-//     })
-//     }
-
-//     const deleteToDo = () => {
-//         const todoRef = firebase.database().ref('crud-app').child(todo.id);
-
-//         todoRef.remove();
-//     }
-    
-//   return (
-//     <Stack className="listItem" direction="horizontal" gap={3}>
-//       <h1 className={todo.complete ? 'complete' : 'incomplete'}>{todo.title}</h1>
-//       <Button onClick={deleteToDo} variant="secondary">Delete</Button>
-//       <div className="vr" />
-//       <Button className={todo.complete ? 'hide-button' : ''} onClick={completeToDo} variant="outline-danger">Complete</Button>
-//       <Button className={todo.complete ? '' : 'hide-button'} onClick={completeToDo} variant="outline-danger">Incomplete</Button>
-//     </Stack>
-//   );
-// }
-
-// export default ToDo;
